3bae0bd81a7d4afdbeef679905d3ed29a1fae39f
RudolfWhicker <rudolf.whicker@gmail.com>
2023-06-21 12:42:03 +0530

    imporved visual experience(?)
    * starfield-animation/app.js:

--- starfield-animation/app.js
+++ starfield-animation/app.js
@@ -4,7 +4,7 @@ let stars = []
 const mainCenter = [(main.clientWidth/2), (main.clientHeight/2)]
 const mainRad = main.clientWidth / 2
 
-console.log(mainCenter)
+let speed = 1.1
 
 function radialSpawn(rad) {
   let theta = Math.random() * 2 * Math.PI
@@ -13,46 +13,55 @@ function radialSpawn(rad) {
   } else {
     rad *= Math.random()
   }
-  return [Math.floor(mainCenter[0] + (Math.cos(theta) * rad)), Math.floor(mainCenter[1] + (Math.sin(theta) * rad)), theta, rad]
+  return [theta, rad]
 }
 
-function radialSpread() {
-  stars.forEach( (starData) => {
-    let star  = starData[0]
-    let theta = starData[1]
-    starData[2] *= 1.05
-    let rad = starData[2]
-    star.style.left = Math.floor(mainCenter[0] + (Math.cos(theta) * rad)) + "px"
-    star.style.top  = Math.floor(mainCenter[1] + (Math.sin(theta) * rad)) + "px"  
+async function radialSpread() {
+  document.querySelectorAll(".line").forEach((line) => {
+    main.removeChild(line)
+  })
+  stars.forEach( (starData) => {    
+    let theta = starData[0]
+    let rad   = starData[1]
+
+    let start   = [Math.cos(theta) * rad, Math.sin(theta) * rad]
+    
+    starData[1] *= speed
+    rad   = starData[1]
+    let end   = [Math.cos(theta) * rad, Math.sin(theta) * rad]
+    let line  = radialLine(start, end, theta)
+    main.appendChild(line)
   })
 }
 
-function killAndRespawn() {
+async function killAndRespawn() {
   stars.forEach( (starData) => {
-    let rad  = starData[2]
+    let rad  = starData[1]
     if (rad >= mainRad) {
-      coordinates = radialSpawn(50)
-      let star = document.createElement("div")
-      star.classList = "star"
-      star.style.left = coordinates[0] + "px"
-      star.style.top  = coordinates[1] + "px"
-      stars.push([star, coordinates[2], coordinates[3]])
-      main.appendChild(star)
+      stars.push(radialSpawn(50))
       stars = stars.filter(item => item != starData)
-      main.removeChild(starData[0])
     }
   })
 }
 
-for (let i = 0; i < 1000; i++) {
-  coordinates = radialSpawn(0)
-  let star = document.createElement("div")
-  star.classList = "star"
-  star.style.left = coordinates[0] + "px"
-  star.style.top  = coordinates[1] + "px"
-  stars.push([star, coordinates[2], coordinates[3]])
-  main.appendChild(star)
+function radialLine(start, end, theta) {
+  let line = document.createElement("div")
+  line.classList = "line"
+  line.style.width  = (1 * Math.sqrt(Math.pow(end[0] - start[0], 2) + Math.pow(end[1] - start[1], 2))) + "px"
+  line.style.left = (end[0] + mainCenter[0]) + "px"
+  line.style.top  = (end[1] + mainCenter[1]) + "px"
+  line.style.transform = "rotate(" + theta + "rad)"
+  
+  return line
 }
+
+for (let i = 0; i < 1000; i++) {
+  stars.push(radialSpawn(0))
+}
+
+main.addEventListener("wheel", (event) => {
+  speed += (event.deltaY - (event.deltaY % 100)) / 1000
+})
 
 setInterval(function() {
   radialSpread()
